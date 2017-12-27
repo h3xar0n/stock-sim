76a8c16250595ce9019b863a6749d08a3619fa1f
Aron Eidelman <aron@eideldev.com>
2017-12-27 10:53:01 -0800

    New modules

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -14,19 +14,27 @@
                             class="form-control"
                             placeholder="Quantity"
                             v-model="quantity"
+                            :class="{danger: insufficientFunds}"
                     >
                 </div>
                 <div class="pull-right">
                     <button
                             class="btn btn-success"
                             @click="buyStock"
-                            :disabled="quantity <= 0"
-                            >Buy</button>
+                            :disabled="insufficientFunds || quantity <= 0"
+                    >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     export default {
@@ -36,6 +44,14 @@
                 quantity: 0
             }
         },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            }
+        },
         methods: {
             buyStock() {
                 const order = {
@@ -43,7 +59,7 @@
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-                console.log(order);
+                this.$store.dispatch('buyStock', order);
                 this.quantity = 0;
             }
         }
