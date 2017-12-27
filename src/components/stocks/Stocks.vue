d9f8ad90e1c6f97e2819797fcd1e8bbee7231e21
Aron Eidelman <aron@eideldev.com>
2017-12-26 16:29:24 -0800

    Stocks component with data for each stock

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>The Stocks Component</h1>
+    <div></div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'BMW', price: 110 },
+                    { id: 2, name: 'Google', price: 200 },
+                    { id: 3, name: 'Apple', price: 250 },
+                    { id: 4, name: 'Twitter', price: 8 }
+                ]
+            }
+        }
+    }
+</script>
